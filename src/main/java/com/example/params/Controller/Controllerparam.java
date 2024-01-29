a08e835e76560971654acc13263eb961a8df4b8d
PearlinsmilyH <pearlinsmily.h@datasirpi.com>
2024-01-29 10:23:54 +0530

    params

---
+++ src/main/java/com/example/params/Controller/Controllerparam.java
@@ -0,0 +1,25 @@
+package com.example.params.Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class Controllerparam
+{
+    //@RequestParam allow you to directly access and extract individual
+    //request parameters without having to handle the entire request body
+    @GetMapping("/request")
+    public String getUsers(@RequestParam String name, @RequestParam(defaultValue = "25") int age)
+    {
+        return "My name is "+name+". I am "+age+" year's old";
+    }
+    //@RequestHeaders are used to transmit additional information
+    //between the client and server in an HTTP request and response
+    @GetMapping("/header")
+    public String handleRequestHeader(@RequestHeader String authorization)
+    {
+        System.out.println("Printing the header "+authorization);
+        return "Success";
+    }
+}
